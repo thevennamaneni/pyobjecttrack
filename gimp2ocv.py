caeeb30b08099149cef0501d0bbdc52ad23f43d7
Sai Narasimha <sainarasimha.v@gmail.com>
2016-09-20 16:39:24 +0530

    Add files via upload

---
+++ gimp2ocv.py
@@ -0,0 +1,11 @@
+print 'Enter H, S, V from GIMP'
+h = float(raw_input())
+s = float(raw_input())
+v = float(raw_input())
+h = h/2
+s = (s/100)*255
+v = (v/100)*255
+
+print h
+print s
+print v
---
+++ sketch_apr22b.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+int incomingByte = 0;
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  myservo.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
+                if (incomingByte == 97) {
+                  pos = pos + 4;
+                }
+                if (incomingByte == 122) {
+                  pos = pos - 4;
+                }
+                pos = constrain(pos, 0, 180);
+                myservo.write(pos);
+                
+}
+}
